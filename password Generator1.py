e68c55c365e408d3ee1948ef5544b56dd15462aa
ali <71112658+awetz@users.noreply.github.com>
2021-03-30 11:53:47 +0430

    Add files via upload

---
+++ password Generator1.py
@@ -0,0 +1,49 @@
+"""
+coded by awetz
+my first script ^_^
+
+"""
+
+import random
+import string
+import os
+
+password = []
+letters = string.ascii_letters
+numbers = string.digits
+symbols = string.punctuation
+
+try:
+    input_letters = int(input("How many letters you want ?: "))
+except:
+    print ("Please enter number!")
+    exit()
+
+try:
+    input_numbers = int(input("How many numbers you want ?: "))
+except:
+    print ("Please enter number!")
+    exit()
+
+try:
+    input_symbol = int(input("How many symbols you want ?: "))
+except:
+    print ("Please enter number!")
+    exit()
+
+for z in range(input_letters):
+    n = random.choice(letters)
+    password.append(n)
+
+for x in range(input_numbers):
+    n = random.choice(numbers)
+    password.append(n)
+
+for c in range(input_symbol):
+    n = random.choice(symbols)
+    password.append(n)
+
+password = "".join(password)
+os.system("clear")
+print("Done !")
+print("Your password is ---> ",password," <---")
